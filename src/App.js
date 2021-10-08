a3c4c92d410de4e786649b499ae5a46498fc185e
Hughes <hughespham2002the2nd@yahoo.com>
2021-10-08 13:33:57 -0500

    tests passed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styled from "styled-components";
+import Character from './components/Character.js'
+import axios from 'axios';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +14,29 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [info, setInfo] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people")
+     .then(res => {
+       setInfo(res.data);
+
+     }).catch(err => {
+       console.error(err);
+     })
+  }, [])
+  console.log(info)
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        info.map(character => {
+          return (
+          <Character info = {character} />
+          )
+        })
+      }
+      
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,50 @@
-// Write your Character component here
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const DropDown = styled.h3`
+font-family: "Comic Sans MS";
+`
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onClick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+export default function Character({info}) {
+    console.log(info)
+    return (
+        <div>
+            <h2>{info.name}</h2>
+            <h3>Gender: {info.gender}</h3>
+            <h3>Height: {info.height}</h3>
+            <h3>Mass: {info.mass}</h3>
+            <h3>Birth Year: {info.birth_year}</h3>
+            <h3>Eye Color: {info.eye_color}</h3>
+            <h3>Hair Color: {info.hair_color}</h3>
+            <h3>Skin Color: {info.skin_color}</h3>
+                <div class="dropdown">
+                    <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+                    <div id="myDropdown" class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                    </div>
+                </div>
+        </div>
+    )
+}
